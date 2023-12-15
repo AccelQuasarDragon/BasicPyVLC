70610168805b39e9263bd0f3ccc06091af3b0722
“AccelQuasarDragon” <138998466+AccelQuasarDragon@users.noreply.github.com>
2023-12-15 11:01:09 -0800

    u

--- basicpyvlc.py
+++ basicpyvlc.py
@@ -1,6 +1,6 @@
 from sys import platform
 
-if "win" in platform: #check if it's windows, ex: it could be win32 or win64
+if platform == "win32": #https://docs.python.org/3/library/sys.html#sys.platform
     import vlc
     import os
     import time
@@ -16,23 +16,26 @@ if "win" in platform: #check if it's windows, ex: it could be win32 or win64
     length = vlc_player.get_length() #time in ms (divide by 1000 to get in seconds)
     newoffset = length/1000 - offset
     time.sleep(newoffset)
-if "darwin" in platform:
+if platform == "darwin":
     #vlc has a problem with mac displaying, see
     #problem description: https://github.com/PySimpleGUI/PySimpleGUI/issues/5581
     #solution: https://stackoverflow.com/a/75022685
     import vlc
     import os
     import time
+    import PySide6.QtWidgets as QtWidgets
 
     vlc_player = vlc.MediaPlayer() 
     medianame = "bigbuckbunny x265.mp4"
     mediapath = os.path.join(os.path.dirname(__file__), medianame)
     media = vlc.Media(mediapath)
     vlc_player.set_media(media)
-    vlc_player.play()
-    offset = 5
-    time.sleep(offset)
-    length = vlc_player.get_length() #time in ms (divide by 1000 to get in seconds)
-    newoffset = length/1000 - offset
-    time.sleep(newoffset)
 
+    vlcApp = QtWidgets.QApplication([])
+    vlcWidget = QtWidgets.QFrame()
+    vlcWidget.resize(700,700)
+    vlcWidget.show()
+
+    vlc_player.set_nsobject(vlcWidget.winId())
+    vlc_player.play() #you need to play vlc first else the qtapp will just open and hold forever
+    vlcApp.exec()

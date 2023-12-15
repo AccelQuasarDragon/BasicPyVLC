d5b4cb25d7d5e830e8739cb1280eda6b49c98b70
“AccelQuasarDragon” <138998466+AccelQuasarDragon@users.noreply.github.com>
2023-12-15 10:27:26 -0800

    u

--- basicpyvlc.py
+++ basicpyvlc.py
@@ -1,16 +1,38 @@
-import vlc
-import os
-import time
+from sys import platform
 
-vlc_player = vlc.MediaPlayer() 
-medianame = "bigbuckbunny x265.mp4"
-mediapath = os.path.join(os.path.dirname(__file__), medianame)
-media = vlc.Media(mediapath)
-vlc_player.set_media(media)
-vlc_player.play()
-offset = 5
-time.sleep(offset)
-length = vlc_player.get_length() #time in ms (divide by 1000 to get in seconds)
-newoffset = length/1000 - offset
-time.sleep(newoffset)
+if "win" in platform: #check if it's windows, ex: it could be win32 or win64
+    import vlc
+    import os
+    import time
+
+    vlc_player = vlc.MediaPlayer() 
+    medianame = "bigbuckbunny x265.mp4"
+    mediapath = os.path.join(os.path.dirname(__file__), medianame)
+    media = vlc.Media(mediapath)
+    vlc_player.set_media(media)
+    vlc_player.play()
+    offset = 5
+    time.sleep(offset)
+    length = vlc_player.get_length() #time in ms (divide by 1000 to get in seconds)
+    newoffset = length/1000 - offset
+    time.sleep(newoffset)
+if "darwin" in platform:
+    #vlc has a problem with mac displaying, see
+    #problem description: https://github.com/PySimpleGUI/PySimpleGUI/issues/5581
+    #solution: https://stackoverflow.com/a/75022685
+    import vlc
+    import os
+    import time
+
+    vlc_player = vlc.MediaPlayer() 
+    medianame = "bigbuckbunny x265.mp4"
+    mediapath = os.path.join(os.path.dirname(__file__), medianame)
+    media = vlc.Media(mediapath)
+    vlc_player.set_media(media)
+    vlc_player.play()
+    offset = 5
+    time.sleep(offset)
+    length = vlc_player.get_length() #time in ms (divide by 1000 to get in seconds)
+    newoffset = length/1000 - offset
+    time.sleep(newoffset)
 
